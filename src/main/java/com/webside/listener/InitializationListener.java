f5ed61e18dd8af7bfc6acdb9971733a8767c9d34
zht1933 <zht1933@163.com>
2017-01-18 20:06:47 +0800

    activiti

--- src/main/java/com/webside/listener/InitializationListener.java
+++ src/main/java/com/webside/listener/InitializationListener.java
@@ -3,6 +3,8 @@
  */
 package com.webside.listener;
 
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
